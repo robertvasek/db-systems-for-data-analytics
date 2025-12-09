1d7c7383bb411a678504e761c3b5e1662c21d795
Robert Vašek <robert.vashek@gmail.com>
2025-12-09 17:16:07 +0100

    FEAT: uploaded hw02 with edited authors.txt

---
+++ hw02/etl-incr.sql
@@ -0,0 +1,13 @@
+-- This script must be able to run as is, i.e., psql <etl-incr.sql
+-- This script may not be "idempotent", so you may not include commands for "new data cleanup"
+
+-- This SQL script that will:
+-- 1/ update the necessary dimension tables
+
+
+
+-- 2/ inserts new facts into tracking
+
+
+
+
